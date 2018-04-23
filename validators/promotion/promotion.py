49061aee9bb1c91cda2323524e55060d8462838a
DoctorJew <thedoctorjew@gmail.com>
2018-04-23 15:34:12 -0700

    Removed VideoValidator.
    VideoValidator was using youtube_dl, which was kind of a pain to have as a dependency. It meant people had to have ffmpeg, and calling other processes isn't very elegant either. Furthermore, the validator was only being used for YouTube URL validation, and we can do this easily with a few API calls. Therefore, youtube_dl is no longer a dependency (isodate is now one however).
    
    We should see some performance gains overall, potentially significant by not having to wait around for YouTubeDL/FFMPEG.
    
    The goal for the future is to try and use existing APIs to parse duration info from the most popular sites posted on the subreddit.

--- validators/promotion/promotion.py
+++ validators/promotion/promotion.py
@@ -1,82 +1,77 @@
-import json
-import mimetypes
 from urllib.parse import urlparse, parse_qs
-import shlex
 import requests
 import isodate
-import subprocess
 from typing import Tuple
 
-import youtube_dl
 from praw.models import Submission
 
 from reddit.enums import Rule, Action
 from reddit.validator import SubmissionValidator
 
 
-class VideoValidator(SubmissionValidator):
-    __slots__ = ['extensions', 'ydl_opts']
-
-    def __init__(self, reddit, quiet: bool = True):
-        super().__init__(reddit)
-        mimetypes.init()
-        self.extensions = self.get_extensions()
-        self.ydl_opts = {'quiet': quiet}
-
-    @staticmethod
-    def get_extensions() -> tuple:
-        extensions = []
-        for ext in mimetypes.types_map:
-            if mimetypes.types_map[ext].split('/')[0] == 'video':
-                extensions.append(ext)
-        return tuple(extensions)
-
-    @staticmethod
-    def get_metadata(input_video):
-        cmd = "ffprobe -v quiet -print_format json -show_streams"
-        args = shlex.split(cmd)
-        args.append(input_video)
-        ffprobe_output = subprocess.check_output(args).decode('utf-8')
-        ffprobe_output = json.loads(ffprobe_output)
-
-        return ffprobe_output
-
-    def find_url(self, data: dict):
-        for k, v in data.items():
-            if isinstance(v, str):
-                if v.endswith(self.extensions):
-                    return v
-            elif isinstance(v, list):
-                d = {}
-                for i in range(len(v)):
-                    d[str(i)] = v[i]
-                return self.find_url(d)
-            elif isinstance(v, dict):
-                return self.find_url(v)
-
-    def validate(self, submission: Submission) -> Tuple[Action, Rule]:
-        url = submission.url
-        meta = None
-        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
-            try:
-                meta = ydl.extract_info(url, download=False)
-            except Exception as e:
-                print(e)
-
-        if meta is not None:
-            if 'duration' in meta and meta['duration'] <= self.config.getfloat('general', 'time_limit'):
-                self.dlog(f'Duration: {meta["duration"]}')
-                return Action.APPROVE, Rule.NONE
-            else:
-                url = self.find_url(meta)
-                try:
-                    metadata = self.get_metadata(url)
-                except Exception as e:
-                    print('ERROR', meta, e)
-                    pass
-                else:
-                    print(metadata)
-        return Action.REMOVE, Rule.PROMOTION
+# class VideoValidator(SubmissionValidator):
+#     __slots__ = ['extensions', 'ydl_opts']
+#
+#     def __init__(self, reddit, quiet: bool = True):
+#         super().__init__(reddit)
+#         mimetypes.init()
+#         self.extensions = self.get_extensions()
+#         self.ydl_opts = {'quiet': quiet}
+#
+#     @staticmethod
+#     def get_extensions() -> tuple:
+#         extensions = []
+#         for ext in mimetypes.types_map:
+#             if mimetypes.types_map[ext].split('/')[0] == 'video':
+#                 extensions.append(ext)
+#         return tuple(extensions)
+#
+#     @staticmethod
+#     def get_metadata(input_video):
+#         cmd = "ffprobe -v quiet -print_format json -show_streams"
+#         args = shlex.split(cmd)
+#         args.append(input_video)
+#         ffprobe_output = subprocess.check_output(args).decode('utf-8')
+#         ffprobe_output = json.loads(ffprobe_output)
+#
+#         return ffprobe_output
+#
+#     def find_url(self, data: dict):
+#         for k, v in data.items():
+#             if isinstance(v, str):
+#                 if v.endswith(self.extensions):
+#                     return v
+#             elif isinstance(v, list):
+#                 d = {}
+#                 for i in range(len(v)):
+#                     d[str(i)] = v[i]
+#                 return self.find_url(d)
+#             elif isinstance(v, dict):
+#                 return self.find_url(v)
+#
+#     def validate(self, submission: Submission) -> Tuple[Action, Rule]:
+#         url = submission.url
+#         meta = None
+#         with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
+#             try:
+#                 meta = ydl.extract_info(url, download=False)
+#             except Exception as e:
+#                 print(e)
+#
+#         if meta is not None:
+#             if 'duration' in meta and meta['duration'] <= self.config.getfloat('general', 'time_limit'):
+#                 self.dlog(f'Duration: {meta["duration"]}')
+#                 return Action.APPROVE, Rule.NONE
+#             else:
+#                 url = self.find_url(meta)
+#                 try:
+#                     metadata = self.get_metadata(url)
+#                 except Exception as e:
+#                     print('ERROR', meta, e)
+#                     pass
+#                 else:
+#                     print(metadata)
+#         return Action.REMOVE, Rule.PROMOTION
 
 
 class YoutubeValidator(SubmissionValidator):
@@ -125,6 +120,11 @@ class PromotionValidator(SubmissionValidator):
     def validate(self, submission: Submission) -> Tuple[Action, Rule]:
         if not any(url in submission.url for url in self.reddit.config.get('domains', 'watched').split(',')):
             return Action.PASS, Rule.NONE
+        elif any(url in submission.url for url in self.reddit.config.get('domains', 'approved'.split(','))):
+            return Action.APPROVE, Rule.NONE
+        elif any(url in submission.url for url in self.reddit.config.get('domains', 'rejected').split(',')):
+            # We're checking domain rule because we don't want to depend on other validators
+            return Action.REMOVE, Rule.DOMAIN
 
         self.dlog('Found watched URL in submission!')
 
